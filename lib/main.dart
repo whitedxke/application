370457ecb05959a3423544f9f2fc4e13c5eefc13
White Dxke <whitedxke@gmail.com>
2024-11-19 23:40:09 +0300

    Initial commit.

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+import 'application.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  SystemChrome.setPreferredOrientations(
+    [
+      DeviceOrientation.portraitUp,
+    ],
+  );
+
+
   runApp(
     const Application(),
   );
-}
-
-class Application extends StatelessWidget {
-  const Application({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container();
-  }
 }
